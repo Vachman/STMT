44d484a05e9a91f60e2dc22c6f132e548e2e598f
Vachman <neyronsoft@gmail.com>
2011-07-11 16:01:56 +0400

    Rename phone_holder to phoneholde and test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110711085319) do
+ActiveRecord::Schema.define(:version => 20110711113555) do
 
   create_table "cars", :force => true do |t|
     t.string   "reg_number"
@@ -68,8 +68,8 @@ ActiveRecord::Schema.define(:version => 20110711085319) do
     t.string   "number"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "phone_holder_id"
-    t.string   "phone_holder_type"
+    t.integer  "phoneholder_id"
+    t.string   "phoneholder_type"
   end
 
   create_table "users", :force => true do |t|
